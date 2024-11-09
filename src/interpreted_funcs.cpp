9008f6cc4777912566e0a685720f12ba166f29db
rjawesome <rohanj2006@gmail.com>
2024-11-08 22:57:49 -0800

    transpose, rows, cols

--- src/interpreted_funcs.cpp
+++ src/interpreted_funcs.cpp
@@ -1,13 +1,13 @@
 #include "interpreted_funcs.h"
 
-variant<string_view, DataContainer> get_matrix_int(DataContainer args[]) {
+variant<string_view, DataContainer> get_matrix(DataContainer args[]) {
     variant<string_view, vector<vector<Fraction>>> matrix = get_matrix();
     return holds_alternative<string_view>(matrix) ? 
         variant<string_view, DataContainer>(get<string_view>(matrix)) :
         DataContainer{TYPE_MATRIX, true, .ptr = new vector<vector<Fraction>>(move(get<vector<vector<Fraction>>>(matrix)))};
 }
 
-variant<string_view, DataContainer> rref_int(DataContainer args[]) {
+variant<string_view, DataContainer> rref(DataContainer args[]) {
     vector<vector<Fraction>> input = *(vector<vector<Fraction>>*)(args[0].ptr);
     variant<string_view, vector<vector<Fraction>>> matrix = rref(input);
     return holds_alternative<string_view>(matrix) ? 
@@ -15,13 +15,13 @@ variant<string_view, DataContainer> rref_int(DataContainer args[]) {
         DataContainer{TYPE_MATRIX, true, .ptr = new vector<vector<Fraction>>(move(get<vector<vector<Fraction>>>(matrix)))};
 }
 
-variant<string_view, DataContainer> solve_equations_int(DataContainer args[]) {
+variant<string_view, DataContainer> solve_equations(DataContainer args[]) {
     vector<vector<Fraction>> input = *(vector<vector<Fraction>>*)(args[0].ptr);
     solve_equations(input);
     return DataContainer{TYPE_NONE, true};
 }
 
-variant<string_view, DataContainer> orthonormalize_int(DataContainer args[]) {
+variant<string_view, DataContainer> orthonormalize(DataContainer args[]) {
     vector<vector<Fraction>> input = *(vector<vector<Fraction>>*)(args[0].ptr);
     variant<string_view, vector<vector<Fraction>>> matrix = orthonormalize_rows(transpose(input));
     if (holds_alternative<vector<vector<Fraction>>>(matrix)) matrix = transpose(get<vector<vector<Fraction>>>(matrix));
@@ -30,7 +30,7 @@ variant<string_view, DataContainer> orthonormalize_int(DataContainer args[]) {
         DataContainer{TYPE_MATRIX, true, .ptr = new vector<vector<Fraction>>(move(get<vector<vector<Fraction>>>(matrix)))};
 }
 
-variant<string_view, DataContainer> qr_int(DataContainer args[]) {
+variant<string_view, DataContainer> qr(DataContainer args[]) {
     vector<vector<Fraction>> input = *(vector<vector<Fraction>>*)(args[0].ptr);
     variant<string_view, vector<vector<Fraction>>> qT_res = orthonormalize_rows(transpose(input));
     if (holds_alternative<string_view>(qT_res)) return get<string_view>(qT_res);
@@ -80,12 +80,30 @@ variant<string_view, DataContainer> mult(DataContainer args[]) {
     return "Multiplication between types not supported";
 }
 
+variant<string_view, DataContainer> transpose(DataContainer args[]) {
+    vector<vector<Fraction>> matrix1 = *(vector<vector<Fraction>>*)(args[0].ptr);
+    return DataContainer{TYPE_MATRIX, true, .ptr = new vector<vector<Fraction>>(move(transpose(matrix1)))};
+}
+
+variant<string_view, DataContainer> rows(DataContainer args[]) {
+    vector<vector<Fraction>> matrix1 = *(vector<vector<Fraction>>*)(args[0].ptr);
+    return DataContainer{TYPE_FRACTION, true, .frac = {(int)matrix1.size(), 1, 1}};
+}
+
+variant<string_view, DataContainer> cols(DataContainer args[]) {
+    vector<vector<Fraction>> matrix1 = *(vector<vector<Fraction>>*)(args[0].ptr);
+    return DataContainer{TYPE_FRACTION, true, .frac = {(int)(matrix1.size() > 0 ? matrix1[0].size(): 0), 1, 1}};
+}
+
 map<string, DataContainer> base_global_frame = {
-    {"get_matrix", {TYPE_FUNCTION, false, .function = {0, {}, TYPE_MATRIX, get_matrix_int} }},
-    {"rref", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_MATRIX, rref_int }}},
-    {"solve_equations", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_NONE, solve_equations_int }}},
-    {"ortho", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_MATRIX, orthonormalize_int }}},
-    {"qr", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_MATRIX, qr_int }}},
+    {"get_matrix", {TYPE_FUNCTION, false, .function = {0, {}, TYPE_MATRIX, get_matrix} }},
+    {"rref", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_MATRIX, rref }}},
+    {"solve_equations", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_NONE, solve_equations }}},
+    {"ortho", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_MATRIX, orthonormalize }}},
+    {"qr", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_MATRIX, qr }}},
+    {"rows", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_FRACTION, rows }}},
+    {"cols", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_FRACTION, cols }}},
+    {"transpose", {TYPE_FUNCTION, false, .function = {1, {TYPE_MATRIX}, TYPE_MATRIX, transpose }}},
     {"+", {TYPE_FUNCTION, false, .function = {2, {TYPE_ANY, TYPE_ANY}, TYPE_ANY, add }}},
     {"*", {TYPE_FUNCTION, false, .function = {2, {TYPE_ANY, TYPE_ANY}, TYPE_ANY, mult }}}
 };
